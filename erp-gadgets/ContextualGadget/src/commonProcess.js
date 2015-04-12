f971ff391c2d1b6bdae823de02905a5aae044907
venkatesan.ece <venkatesan.ece@gmail.com>
2015-04-12 05:10:05 +0000

    Chnaged

--- erp-gadgets/ContextualGadget/src/commonProcess.js
+++ erp-gadgets/ContextualGadget/src/commonProcess.js
@@ -429,6 +429,7 @@ function emailLoginrequest() {
    alert("inside login request Login Name :"+prefs.getString("LoginName"));
    alert("inside login request Session Expire time :"+prefs.getString("LoginExpire"));
    alert("inside login request System Time:"+sessionTime);
+   prefs.setString("LoginExpire","");
 	var str1=prefs.getString("LoginExpire");
 	var str2=sessionTime;
 	var monthval=str1.substring(0,str1.indexOf("/"));
