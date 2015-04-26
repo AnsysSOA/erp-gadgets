1addb3b5dd9d3bcab9b6ddaf375ec7e6b31fe7ae
venkatesan.ece <venkatesan.ece@gmail.com>
2015-04-26 15:31:21 +0000

    Chanegd

--- erp-gadgets/ContextualGadget/Sandbox/SandboxSVN/src/commonProcess.js
+++ erp-gadgets/ContextualGadget/Sandbox/SandboxSVN/src/commonProcess.js
@@ -479,13 +479,13 @@ function emailLoginrequest() {
 prefs.set("LoginExpire",null);
 	    	  if(date2>date1||prefs.getString("LoginExpire")==null||prefs.getString("LoginExpire")=="")
 	    		  {
-    	  //   alert("inside login request if condition System Time:"+sessionTime);
+    	    alert("inside login request if condition System Time:"+sessionTime);
 
 	    		  	$(".debugVal").hide('fast');
 	    		  	$(".msg_list").hide('fast');
 	    		  	gadgets.window.adjustHeight(0);
-	    		 // 	alert("inside request method");
-           //      alert("inside request method url :"+url);
+	    		  	alert("inside request method");
+                 alert("inside request method url :"+authURL);
 	    		  	var params = {};
 	    		    // params[gadgets.io.RequestParameters.CONTENT_TYPE] = gadgets.io.ContentType.TEXT;  -- Commented By Venkatesan
                  params[gadgets.io.RequestParameters.CONTENT_TYPE] = gadgets.io.ContentType.JSON  // Added By Venkatesan
